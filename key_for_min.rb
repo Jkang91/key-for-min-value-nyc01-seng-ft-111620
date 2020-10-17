e1e315ed1849ad1ae6032027ce60c8e301d4fb3b
June Young Kang <jkang34@gmail.com>
2020-10-17 18:39:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 1000
   name_hash.each do |key, value|
     if min_value > value
-      min_value = :key 
-      
+      min_value = value 
     end 
   end
   min_value
